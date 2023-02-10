aa8c19665211dd03f77626679cb8e9c3f6e35e39
Lilis Kurniasari <liliskurnia548@gmail.com>
2023-02-10 09:21:32 +0700

    keycloak.js

--- src/components/keycloak.js
+++ src/components/keycloak.js
@@ -2,7 +2,7 @@ import Keycloak from 'keycloak-js'
 
 const keycloak = new Keycloak({
      realm: "DUKCAPIL-Raihan",
-     url: "https://redhat-sso-bkkbn.apps.tkp.platform.lintasarta.net",
+     url: "https://redhat-sso-bkkbn.apps.tkp.platform.lintasarta.net/auth/",
      clientId: "BPJS-Raihan"
   })
 
