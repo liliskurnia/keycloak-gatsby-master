2bf3bff67bb33548b6ed39a1db57dbda9d36ac8a
Lilis Kurniasari <liliskurnia548@gmail.com>
2023-02-13 20:45:07 +0700

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,11 @@
 import React from "react"
 import { useKeycloak } from "@react-keycloak/web"
-import Login from "./inputToken"
 
 const Home = () => {
   const { keycloak, initialized } = useKeycloak()
   
   return (
     <>
-    <Login/>
       <div>The user is {keycloak.authenticated ? "" : "NOT"} authenticated</div>
       {keycloak.authenticated && (
         <button type="button" onClick={() => keycloak.logout()}>
