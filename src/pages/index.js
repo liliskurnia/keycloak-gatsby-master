e0aad5d943068ec143a3b8269d681c36e53d170a
Lilis Kurniasari <liliskurnia548@gmail.com>
2023-02-13 14:04:02 +0700

    no message

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,21 +3,6 @@ import {useKeycloak} from '@react-keycloak/web'
 
 const Home = () => {
   const {keycloak, initialized} = useKeycloak()
-  var url = "https://keycloakgatsbymastermain.gatsbyjs.io/";
-
-var xhr = new XMLHttpRequest();
-xhr.open("GET", url);
-
-xhr.setRequestHeader("Accept", "application/json");
-xhr.setRequestHeader("Authorization", "Bearer {access_token}");
-
-xhr.onreadystatechange = function () {
-   if (xhr.readyState === 4) {
-      console.log(xhr.status);
-      console.log(xhr.responseText);
-   }};
-
-xhr.send();
   
 return (<>
 <div>The user is {keycloak.authenticated ? '' : 'NOT'} authenticated</div>
